d076cf61fd9c7e73385f1cfdfc4647a574f3e1a9
sharmeen <sharmeen1209@gmail.com>
2023-01-21 17:26:12 +0530

    Comments Added

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ location = input("Enter the city name: ")
 complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
 api_link = requests.get(complete_api_link)
 api_data = api_link.json()
-# print(api_data)
 
-#create variables to store and display data
+# Create variables to store and display data
+# Convert farenheit to celcius
 temp_city = ((api_data['main']['temp']) - 273.15)
 weather_desc = api_data['weather'][0]['description']
 humid = api_data['main']['humidity']
 wind_spd = api_data['wind']['speed']
+# Get Current Date time
 date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
 print (f"Weather Report for - {location.upper()}  || {date_time}\n")
